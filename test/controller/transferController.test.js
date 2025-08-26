ce37ad811d22f72aa782751913ca9079f7358d84
allexandresampaio <allexandresss@gmail.com>
2025-08-26 10:34:44 -0300

    moving folders

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -1,26 +1,27 @@
 //Bibliotecas
 const request = require('supertest');
 const sinon = require('sinon');
+const { expect } = require('chai');
 
 //Aplicação
-const app = require('../../app');
+const app = require('../../src/app');
 
 //Testes
 describe('Transfer Controller', () => {
-    describe('POST, /transfers', () => {
+    describe('POST, /api/transfer', () => {
         it('Quando informo origem e destino inexistentes, o retorno será 400', async () => {
             const resposta = await request(app)
-                .post('/transfers')
+                .post('/api/transfer')
                 .send({
                     from: "alle",
                     to: "desa",
-                    value: 50
+                    amount: 50
                 });
             expect(resposta.status).to.equal(400);
         });
     });
 
-    describe('GET, /transfers', () => {
+    describe('GET, /api/transfer', () => {
 
     });
 })
