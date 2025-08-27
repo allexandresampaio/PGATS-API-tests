abe72bbc65900ae4e8fbb600ade00c4e825ed61c
allexandresampaio <allexandresss@gmail.com>
2025-08-27 10:35:02 -0300

    criando início dos mocks com sinon

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,6 +6,9 @@ const { expect } = require('chai');
 //Aplicação
 const app = require('../../src/app');
 
+//Mock
+const transferService = require('../../src/services/transferService');
+
 //Testes
 describe('Transfer Controller', () => {
     describe('POST, /api/transfer', () => {
@@ -19,6 +22,25 @@ describe('Transfer Controller', () => {
                 });
             expect(resposta.status).to.equal(400);
             expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+        });
+
+        //Mockar apenas a funcao transfer
+        it('Usando Mocks: Quando informo origem e destino inexistentes, o retorno será 400', async () => {
+            const transferServiceMock = sinon.stub(transferService, 'transfer');
+            transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado'));
+
+            const resposta = await request(app)
+                .post('/api/transfer')
+                .send({
+                    from: "alle",
+                    to: "desa",
+                    amount: 50
+                });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+
+            //Resete o mock
+            sinon.restore();
         });
     });
 
