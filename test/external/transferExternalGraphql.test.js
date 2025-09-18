2368e2e65e29fb2d2a92573541abf215bba4e695
allexandresampaio <allexandresss@gmail.com>
2025-09-18 12:04:55 -0300

    criando novo teste e usando beforeEach pra token

--- test/external/transferExternalGraphql.test.js
+++ test/external/transferExternalGraphql.test.js
@@ -2,7 +2,10 @@ const request = require('supertest');
 const { expect } = require('chai');
 
 describe('Testes de Transferência - Graphql', () => {
-    it('Validar que é possível transferir entre duas contas', async () => {
+
+    let tokenCapturado = null;
+
+    beforeEach(async () => {
         //capturar o token
         const respostaLogin = await request('http://localhost:4000')
         .post('/graphql')
@@ -19,12 +22,15 @@ describe('Testes de Transferência - Graphql', () => {
             }
         })
 
-        const tokenCapturado = respostaLogin.body.data.login.token
-        //console.log(tokenCapturado)
+        this.tokenCapturado = respostaLogin.body.data.login.token
+        //console.log(this.tokenCapturado);
+    })
 
+    it('Validar que é possível transferir entre duas contas', async () => {
+        
         const respostaTransferencia = await request('http://localhost:4000')
         .post('/graphql')
-        .set('Authorization', `Bearer ${tokenCapturado}`)
+        .set('Authorization', `Bearer ${this.tokenCapturado}`)
         .send({
             query: `
                 mutation Transfer($from: String!, $to: String!, $amount: Float!) {
@@ -41,6 +47,37 @@ describe('Testes de Transferência - Graphql', () => {
                 amount: 312
             }
         })
-        expect(respostaTransferencia.status).to.equal(200);    
+        expect(respostaTransferencia.status).to.equal(200);
+        expect(respostaTransferencia.body.data.transfer).to.include({
+            from: 'alle',
+            to: 'desa',
+            amount: 312
+        });
+        expect(respostaTransferencia.body.data.transfer.date).to.exist;
+    });
+
+    it('Validar que não é possível realizar transferência com valor acima do saldo em conta', async () => {
+        const respostaTransferencia = await request('http://localhost:4000')
+        .post('/graphql')
+        .set('Authorization', `Bearer ${this.tokenCapturado}`)
+        .send({
+            query: `
+                mutation Transfer($from: String!, $to: String!, $amount: Float!) {
+                    transfer(from: $from, to: $to, amount: $amount) {
+                        from
+                        to
+                        amount
+                        date
+                    }
+                } `,
+            variables: {
+                from: 'alle',
+                to: 'desa',
+                amount: 10000.01
+            }
+        })
+        //console.log(respostaTransferencia.body.errors[0].message);
+        expect(respostaTransferencia.status).to.equal(200);  
+        expect(respostaTransferencia.body.errors[0].message).to.equal('Saldo insuficiente');
     });
 });
